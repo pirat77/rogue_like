600c1dbeaed73298fb052d733d2559aac5860e3b
pirat77 <mcyanek@gmail.com>
2020-02-17 10:33:50 +0100

    removed magic numbers

--- rogue.py
+++ rogue.py
@@ -12,6 +12,8 @@ def getch():
     return ch
 
 def print_map(x, y, player_position):
+    POSITION_X = 0
+    POSITION_Y = 1
     os.system('clear')
     map = ''
     for i in range(x):
@@ -19,7 +21,7 @@ def print_map(x, y, player_position):
             if i == 0 or i == y - 1 or j == 0 or j == x - 1: 
                 map += '#'
             else:
-                if player_position[0] == i and player_position[1] == j:
+                if player_position[POSITION_X] == i and player_position[POSITION_Y] == j:
                     map += '@'
                 else:
                     map += '.'
@@ -30,11 +32,12 @@ def print_map(x, y, player_position):
 def main():
     DIRRECTIONS = {'w': [-1, 0], 's': [1, 0], 'a': [0, -1], 'd': [0, 1]}
     player_position = [3, 3]
-    LVL_SIZE = 15
+    LVL_X = 15
+    LVL_Y = 15
     life = 3
     
     while life > 0:
-        print_map(LVL_SIZE, LVL_SIZE, player_position)
+        print_map(LVL_X, LVL_Y, player_position)
         button = getch()
         for x in range(len(player_position)):
             player_position[x] += DIRRECTIONS[button][x]
